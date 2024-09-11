01a6c19523c9ad10ae560681aa6dc3315eb0e56e
TarikAguen <98916020+TarikAguen@users.noreply.github.com>
2024-09-11 19:56:35 +0200

    test deploy

--- dist/middleware/auth.js
+++ dist/middleware/auth.js
@@ -1,37 +1,29 @@
 "use strict";
-var __importDefault =
-  (this && this.__importDefault) ||
-  function (mod) {
-    return mod && mod.__esModule ? mod : { default: mod };
-  };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.authenticateJWT = void 0;
 const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
 const auth_1 = require("../routes/auth");
 const authenticateJWT = (req, res, next) => {
-  var _a;
-  const token =
-    (_a = req.header("Authorization")) === null || _a === void 0
-      ? void 0
-      : _a.split(" ")[1];
-  if (token) {
-    if (auth_1.revokedTokens.has(token)) {
-      return res.status(401).send("Token has been revoked");
-    }
-    jsonwebtoken_1.default.verify(
-      token,
-      process.env.JWT_SECRET,
-      (err, user) => {
-        if (err) {
-          console.log(err);
-          return res.sendStatus(403);
+    var _a;
+    const token = (_a = req.header("Authorization")) === null || _a === void 0 ? void 0 : _a.split(" ")[1];
+    if (token) {
+        if (auth_1.revokedTokens.has(token)) {
+            return res.status(401).send("Token has been revoked");
         }
-        res.locals.user = user;
-        next();
-      }
-    );
-  } else {
-    res.sendStatus(401);
-  }
+        jsonwebtoken_1.default.verify(token, process.env.JWT_SECRET, (err, user) => {
+            if (err) {
+                console.log(err);
+                return res.sendStatus(403);
+            }
+            res.locals.user = user;
+            next();
+        });
+    }
+    else {
+        res.sendStatus(401);
+    }
 };
 exports.authenticateJWT = authenticateJWT;
--- dist/server.js
+++ dist/server.js
@@ -1,9 +1,7 @@
 "use strict";
-var __importDefault =
-  (this && this.__importDefault) ||
-  function (mod) {
-    return mod && mod.__esModule ? mod : { default: mod };
-  };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const mongoose_1 = __importDefault(require("mongoose"));
@@ -18,14 +16,14 @@ const PORT = process.env.PORT || 3001;
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
 mongoose_1.default
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.log(err));
+    .connect(process.env.MONGO_URI)
+    .then(() => console.log("MongoDB connected"))
+    .catch((err) => console.log(err));
 app.get("/", (req, res) => {
-  res.send("Wesh le sang!");
+    res.send("Wesh le sang!");
 });
 app.use("/auth", auth_2.default);
 app.use("/profil", auth_1.authenticateJWT, profil_1.default);
-app.listen(() => {
-  console.log(`Server running`);
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
 });
