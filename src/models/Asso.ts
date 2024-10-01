af052143ef1d4630b3d057da2a4a04ad94e87e13
TarikAguen <98916020+TarikAguen@users.noreply.github.com>
2024-10-01 14:22:49 +0200

    resolve models asso

--- src/models/Asso.ts
+++ src/models/Asso.ts
@@ -4,14 +4,14 @@ interface IAsso extends Document {
   email: string;
   password: string;
   nameasso?: string;
-  siret?: string;
+  siret?: number;
   logo?: string;
   description?: string;
   presentation?: string;
   website?: string;
-  telephone?: string;
+  telephone?: number;
   location?: string;
-  creationdate?: string;
+  creationdate?: Date;
   interests?: string[];
 }
 
@@ -20,14 +20,14 @@ const AssoSchema: Schema = new Schema(
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     nameasso: { type: String, required: true },
-    siret: { type: String, required: true },
+    siret: { type: Number, required: true },
     logo: { type: String },
     location: { type: String, required: true },
-    telephone: { type: String, required: true },
+    telephone: { type: Number, required: true },
     description: { type: String },
     presentation: { type: String },
-    website: { type: Date },
-    creationdate: { type: String },
+    website: { type: String },
+    creationdate: { type: Date },
     interests: { type: [String] },
   },
   { collection: "asso" }
