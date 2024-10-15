00fd19507532915751743d0b47451a38239cc958
TarikAguen <98916020+TarikAguen@users.noreply.github.com>
2024-10-15 21:01:52 +0200

    Update asso.ts

--- src/routes/asso.ts
+++ src/routes/asso.ts
@@ -1,15 +1,17 @@
 import { Router } from "express";
+import multer from "multer";
 import {
   updateAsso,
   profilUpdate,
   getAssoDetails,
   deleteAssoProfil,
   getAllAsso,
-} from "../controller/update-asso-controller"; // Importation du contrôleur
+} from "../controller/update-asso-controller";
+const upload = multer({ storage: multer.memoryStorage() }); // Importation du contrôleur
 
 const router = Router();
 
-router.put("/update-asso", updateAsso); // Route qui appelle la fonction du contrôleur
+router.put("/update-asso", upload.single("logo"), updateAsso); // Route qui appelle la fonction du contrôleur
 router.post("/profilupdate", profilUpdate); // Route qui appelle la fonction du contrôleur
 router.get("/asso-details", getAssoDetails); // Renvoie les details de l'asso
 router.delete("/delete", deleteAssoProfil); // Route qui supprime l'asso
