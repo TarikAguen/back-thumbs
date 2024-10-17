a563036fc5c5f273862be6785b51870fbfab9799
TarikAguen <98916020+TarikAguen@users.noreply.github.com>
2024-10-18 01:19:22 +0200

    Update event.ts

--- src/routes/event.ts
+++ src/routes/event.ts
@@ -18,7 +18,7 @@ const upload = multer({ storage: multer.memoryStorage() });
 router.post("/create-event", upload.single("photo"), createEvent);
 router.get("/getUser-event", getUserEvents);
 router.get("/events", getAllEvents);
-router.post("/:id/toggle-participant", toggleParticipant);
+router.post("/toggle-participant/:id", toggleParticipant);
 // Route pour filtrer les événements par intérêts
 router.get("/event-filter", filterByInterestsAndType);
 router.get("/getEvent/:id", getEventById); // Fonction pour récupérer un événement par ID
