d3672fb7513bc91221aaa3bbc575f71e1bab8fc7
TarikAguen <98916020+TarikAguen@users.noreply.github.com>
2024-10-19 06:39:47 +0200

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -9,8 +9,7 @@ import profilRoutes from "./routes/profil";
 import AssoRoutes from "./routes/asso";
 import EventRoutes from "./routes/event";
 import GeoRoutes from "./routes/geocode";
-import Message from "../src/models/Message";
-import { Server, createServer } from "http";
+import { Server } from "http";
 import { Server as SocketIOServer, Socket } from "socket.io";
 import messageRoutes from "./routes/message";
 // import PasswordRoutes from "./routes/reset-password";
@@ -18,44 +17,42 @@ import { authenticateJWTAsso } from "./middleware/auth-asso";
 dotenv.config();
 const app: Express = express();
 const server = new Server(app);
-const io = new SocketIOServer(server);
-
+const io = new SocketIOServer(server, {
+  cors: {
+    origin: "*", // Permettre les requêtes depuis n'importe quelle origine
+    methods: ["GET", "POST"],
+  },
+});
+// Configuration de Socket.IO
 io.on("connection", (socket: Socket) => {
   console.log("New client connected");
 
   const userId = socket.handshake.query.userId as string | undefined;
 
   if (userId) {
-    socket.join(userId); // Chaque utilisateur rejoint une "room" avec son propre ID
+    socket.join(userId); // L'utilisateur rejoint une "room" basée sur son propre ID
     console.log(`User ${userId} joined their own room`);
   } else {
     console.log("No userId provided, cannot join private room");
   }
 
-  // Écouter les messages envoyés depuis le frontend
-  socket.on("send_message", async (data) => {
+  // Recevoir un message depuis le frontend via Socket.IO
+  socket.on("send_message", (data) => {
     const { senderId, receiverId, content } = data;
 
-    // Stocker le message dans la base de données via votre contrôleur
-    const message = new Message({
-      sender: senderId,
-      receiver: receiverId,
+    // Envoyer le message aux deux utilisateurs via leur "room"
+    io.to(senderId).emit("receive_message", { senderId, receiverId, content });
+    io.to(receiverId).emit("receive_message", {
+      senderId,
+      receiverId,
       content,
-      sentAt: new Date(),
-      onModel: "User", // Modifier si nécessaire selon vos modèles
     });
-    await message.save();
-
-    // Émettre le message aux deux utilisateurs impliqués
-    io.to(senderId).emit("receive_message", message); // Envoyer à l'expéditeur
-    io.to(receiverId).emit("receive_message", message); // Envoyer au destinataire
   });
 
   socket.on("disconnect", () => {
     console.log("Client disconnected");
   });
 });
-
 const PORT: string | number = process.env.PORT || 3001;
 app.use(cors());
 app.use(express.json());
